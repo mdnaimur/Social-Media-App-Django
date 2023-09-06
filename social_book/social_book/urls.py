ef3c5da70482623cf59de11fb674cfee937e22b5
mdnaimur <mdnaimurr@gmail.com>
2023-09-06 21:32:36 +0600

    Profile model created

--- social_book/social_book/urls.py
+++ social_book/social_book/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('',include('core.urls'))
 ]
 
-# urlpatterns = urlpatterns = [
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
     
-# ]
+
