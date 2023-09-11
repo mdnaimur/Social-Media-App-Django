8338861777c2a1bda0d85a92f565c6d8e6a25931
mdnaimur <mdnaimurr@gmail.com>
2023-09-11 12:44:53 +0600

    upload post coding

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,16 +1,31 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from datetime import datetime
+import uuid
 
 User = get_user_model()
 # Create your models here.
 
+
 class Profile(models.Model):
     id_user = models.IntegerField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    profile_img = models.ImageField(upload_to='profile_images',default = 'lake.jpg')
+    profile_img = models.ImageField(
+        upload_to='profile_images', default='lake.jpg')
     location = models.CharField(max_length=100, blank=True)
-    
+
     def __str__(self):
         return self.user.username
-    
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
