1b232bdad5304be986432f4d50ebece2b1aff448
mdnaimur <mdnaimurr@gmail.com>
2023-09-11 14:41:28 +0600

    like post updated

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
+    path('like-post', views.like_post, name='like-post'),
     path('upload', views.upload, name='upload'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
